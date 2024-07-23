cbc15108cef6c96d22a1dd647c988782a32b00f8
difamuflih <difamuflih@gmail.com>
2024-07-23 16:47:59 +0700

    ui creator

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -2,32 +2,21 @@
 
     <div class="rounded-lg dark:border-gray-700">
         <div class="bg-white dark:bg-gray-900 pt-4 sm:ml-64 h-screen">
-            <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-                <h2 class="my-4 text-xl font-bold text-gray-900 dark:text-white">Add a new product</h2>
+            <div class="my-20 py-4 px-4  mx-auto max-w-2xl lg:py-8 bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg">
+                <h2 class="my-4 text-xl font-bold text-gray-900 dark:text-white">Add a new Category</h2>
                 <form action="#">
                     <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                         <div class="sm:col-span-2">
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
-                        </div>
-                        <div class="w-full">
-                            <label for="photo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Photo</label>
-                            <input type="file" name="photo" id="photo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 " placeholder="Upload photo" required="">
-                        </div> 
-                        <div class="w-full">
-                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a Category</label>
-                            <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <option selected>Category</option>
-                                <option value="US">Education</option>
-                            </select>
+                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                            <input type="text" name="category" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type product category" required="">
                         </div>
                         <div class="sm:col-span-2">
-                            <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">News</label>
-                            <textarea id="description" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
-                        </div>
+                            <label for="icon" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Icon</label>
+                            <input type="file" name="icon" id="icon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Upload icon" required="">
+                        </div> 
                     </div>
-                    <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                        Add product
+                    <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                        Add Category
                     </button>
                 </form>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,18 @@ Route::get('/create', function () {
     return view('admin.create');
 });
 
+Route::get('/home', function () {
+    return view('user.home');
+});
+
+Route::get('/news', function () {
+    return view('user.news');
+});
+
+Route::get('/category', function () {
+    return view('user.category');
+});
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
